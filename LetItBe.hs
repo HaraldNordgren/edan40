fc892557e11a08919ec6b72ae874790716f9524f
HaraldNordgren <haraldnordgren@gmail.com>
2015-07-03 20:49:22 +0200

    project 2: preparing to add to the bigger repo

--- LetItBe.hs
+++ LetItBe.hs
@@ -7,7 +7,7 @@ lmap f l = line (map f l)
 
 letitbeMelody = line [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12]
 
-v1 = lmap vol [g 5 sn, g 5 sn, g 5 en, g 5 qn]
+v1 = lmap vol [g 5 qn, g 5 qn, g 5 qn, g 5 qn]
 v2 = lmap vol [a 5 en, e 5 en, g 5 qn, g 5 en]
 v3 = lmap vol [c 6 sn, d 6 sn, e 6 sn, e 6 sn]
 v4 = lmap vol [e 6 qn, d 6 en, d 6 qn, c 6 sn]
